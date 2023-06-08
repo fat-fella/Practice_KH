17acc3a3d83d208149873edd84621fae4855f469
fat-fella <aznclee711@gmail.com>
2023-06-08 17:52:32 +0900

    compex

---
+++ Compex_Practice6/src/com/kh/practice/comp/run/Run.java
@@ -0,0 +1,18 @@
+package com.kh.practice.comp.run;
+
+import com.kh.practice.comp.func.CompExample;
+import com.kh.practice.comp.func.UpandDown;
+
+public class Run {
+
+	public static void main(String[] args) {
+		CompExample ce = new CompExample();
+//		ce.practice1();
+//		ce.practice2();
+//		ce.practice3();
+//		ce.practice4();
+		UpandDown ud = new UpandDown();
+		ud.upDown();
+	}
+
+}
