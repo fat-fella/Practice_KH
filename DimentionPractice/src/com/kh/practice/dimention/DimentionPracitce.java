152535b86b1941a30edb207fe74a470333cee845
fat-fella <aznclee711@gmail.com>
2023-06-09 18:47:10 +0900

    io practice 1

---
+++ DimentionPractice/src/com/kh/practice/dimention/DimentionPracitce.java
@@ -0,0 +1,14 @@
+package com.kh.practice.dimention;
+
+public class DimentionPracitce {
+	public void practice1() {
+		int[][] arr = new int[3][3];
+
+		for (int i = 0; i < arr.length; i++) {
+			for(int j=0; j<arr.length; j++) {
+				
+				System.out.println(arr[i][j]);
+			}
+		}
+	}
+}
---
+++ DimentionPractice/src/com/kh/practice/run/Run.java
@@ -0,0 +1,12 @@
+package com.kh.practice.run;
+
+import com.kh.practice.dimention.DimentionPracitce;
+
+public class Run {
+
+	public static void main(String[] args) {
+		DimentionPracitce cp = new DimentionPracitce();
+		cp.practice1();
+	}
+
+}
